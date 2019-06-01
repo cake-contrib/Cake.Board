cb203e8f799e0f038069b4b04bfd9618f17d2742
Nicola Biancolini <nicola.biancolini@reactconsulting.it>
2019-06-01 18:21:12 +0200

    setup

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Nicola Biancolini, 2019. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyTrademark("")]
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
