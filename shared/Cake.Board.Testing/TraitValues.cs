ae50991a99f9a88ef6723fc50a5a02194f8d0c84
Nicola Biancolini <nicola.biancolini@reactconsulting.it>
2019-06-12 10:55:36 +0200

    add ruleset for test projects

---
+++ shared/Cake.Board.Testing/TraitValues.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Nicola Biancolini, 2019. All rights reserved.
+// Licensed under the MIT license. See the LICENSE file in the project root for full license information.
+
+namespace Cake.Board.Testing
+{
+    /// <summary>
+    /// xUnit trait values.
+    /// </summary>
+    public class TraitValues
+    {
+        /// <summary>
+        /// Trait value for unit test.
+        /// </summary>
+        public const string UNIT_TEST = "Unit test";
+
+        /// <summary>
+        /// Trait value for acceptance test.
+        /// </summary>
+        public const string ACCEPTANCE_TEST = "Acceptance test";
+    }
+}
--- test/Cake.Board.Tests/Units/EnsureUnit.cs
+++ test/Cake.Board.Tests/Units/EnsureUnit.cs
@@ -4,29 +4,104 @@
 using System;
 
 using Cake.Board.Extensions;
+using Cake.Board.Testing;
 using Xunit;
 
 namespace Cake.Board.Tests.Units
 {
-    /// <summary>
-    /// Todo.
-    /// </summary>
     public class EnsureUnit
     {
-        /// <summary>
-        /// Todo.
-        /// </summary>
         [Fact]
+        [Trait(TraitNames.TEST_CATEGORY, TraitValues.UNIT_TEST)]
         public void ScenarioWithArgumentNull()
         {
             // Arrange
             var argument = default(Exception);
+            string message = "lorem ipsum";
 
             // Act
-            Exception record = Record.Exception(() => argument.NotNull("lorem ipsum"));
+            Exception record = Record.Exception(() => argument.NotNull(message));
 
             // Assert
             Assert.IsType<ArgumentNullException>(record);
+            Assert.Equal(new ArgumentNullException(message).Message, record.Message);
+        }
+
+        [Fact]
+        [Trait(TraitNames.TEST_CATEGORY, TraitValues.UNIT_TEST)]
+        public void ScenarioWithArgumentNotNull()
+        {
+            // Arrange
+            var argument = new Exception();
+            string message = "lorem ipsum";
+
+            // Act
+            Exception record = Record.Exception(() => argument.NotNull(message));
+
+            // Assert
+            Assert.Null(record);
+        }
+
+        [Fact]
+        [Trait(TraitNames.TEST_CATEGORY, TraitValues.UNIT_TEST)]
+        public void ScenarioWithArgumentEmpty()
+        {
+            // Arrange
+            string argument = string.Empty;
+            string message = "lorem ipsum";
+
+            // Act
+            Exception record = Record.Exception(() => argument.ArgumentNotEmpty(message));
+
+            // Assert
+            Assert.IsType<ArgumentException>(record);
+            Assert.Equal(new ArgumentException(message).Message, record.Message);
+        }
+
+        [Fact]
+        [Trait(TraitNames.TEST_CATEGORY, TraitValues.UNIT_TEST)]
+        public void ScenarioWithArgumentNotEmpty()
+        {
+            // Arrange
+            string argument = "lorem ipsum";
+            string message = "lorem ipsum";
+
+            // Act
+            Exception record = Record.Exception(() => argument.ArgumentNotEmpty(message));
+
+            // Assert
+            Assert.Null(record);
+        }
+
+        [Fact]
+        [Trait(TraitNames.TEST_CATEGORY, TraitValues.UNIT_TEST)]
+        public void ScenarioWithArgumentWithWhitespace()
+        {
+            // Arrange
+            string argument = " ";
+            string message = "lorem ipsum";
+
+            // Act
+            Exception record = Record.Exception(() => argument.ArgumentNotEmptyOrWhitespace(message));
+
+            // Assert
+            Assert.IsType<ArgumentException>(record);
+            Assert.Equal(new ArgumentException(message).Message, record.Message);
+        }
+
+        [Fact]
+        [Trait(TraitNames.TEST_CATEGORY, TraitValues.UNIT_TEST)]
+        public void ScenarioWithArgumentWithoutWhitespace()
+        {
+            // Arrange
+            string argument = "lorem ipsum";
+            string message = "lorem ipsum";
+
+            // Act
+            Exception record = Record.Exception(() => argument.ArgumentNotEmptyOrWhitespace(message));
+
+            // Assert
+            Assert.Null(record);
         }
     }
 }
